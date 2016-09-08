97d44b149589dce6d22d3b9213efe38fd4f11e4a
Donovan Brady <donovanbrady@lw-admins-MacBook-Pro-2.local>
2016-09-08 15:07:33 -0400

    Modified dbr-ri to include a no-tags flag.

--- zephyr/plugins/etl/dbr_ri.py
+++ zephyr/plugins/etl/dbr_ri.py
@@ -10,18 +10,24 @@ class ToolkitETLController(controller.CementBaseController):
         stacked_type = "nested"
         description = "Generate single table reports for an account."
         arguments = controller.CementBaseController.Meta.arguments + [(
-            ["--infile"], dict(
-                type=str,
-                help="Path to input file",
-                required=True,
+                ["--infile"], dict(
+                    type=str,
+                    help="Path to input file.",
+                    required=True,
+                ),
             ),
-        ),
-        (
-            ["--outfile"], dict(
-                type=str,
-                help="Path to output file",
-                required=True,
-            )
+            (
+                ["--outfile"], dict(
+                    type=str,
+                    help="Path to output file.",
+                    required=True,
+                ),
+            ),
+            (
+               ["--no-tags"], dict(
+                    action="store_true",
+                    help="Removes tags in output file."
+                )
         )]
 
     @controller.expose(hide=True)
@@ -45,13 +51,19 @@ class ToolkitFilterDBR(ToolkitETLController):
     def run(self, **kwargs):
         infile = self.app.pargs.infile
         outfile = self.app.pargs.outfile
+        no_tags = self.app.pargs.no_tags
         self.app.log.info("Using input file: {infile}".format(infile=infile))
         self.app.log.info("Using output file: {outfile}".format(outfile=outfile))
         with open(infile, "r") as dbrin, open(outfile, "w") as dbrout:
             reader = csv.DictReader(dbrin)
-            headers = reader.fieldnames
-            writer = csv.DictWriter(dbrout, fieldnames=headers, quoting=csv.QUOTE_ALL)
+            header = reader.fieldnames
+            if no_tags:
+                header = [col for col in header if ":" not in col]
+            print(header)
+            writer = csv.DictWriter(dbrout, fieldnames=header, quoting=csv.QUOTE_ALL)
             writer.writeheader()
             for row in reader:
-                if row['ReservedInstance'] == 'Y':
-                    writer.writerow(row)
+                if row["ReservedInstance"] != "Y":
+                    continue
+                out = {col:row[col] for col in header}
+                writer.writerow(out)

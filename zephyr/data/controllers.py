05ce8a4994fff9cfd742cc937b93d76326a6916f
Dakota Blair <dblair@logicworks.net>
2016-11-28 22:21:08 +0100

    Unified controller behavior in the run_call method of DataRun. Removed WarpRun controller.

--- zephyr/data/controllers.py
+++ zephyr/data/controllers.py
@@ -70,8 +70,7 @@ class DataRun(ZephyrData):
     def default(self):
         self.run(**vars(self.app.pargs))
 
-class WarpRun(DataRun):
-    def warp_run(self, WarpClass, **kwargs):
+    def run_call(self, cls, **kwargs):
         account = self.app.pargs.account
         cache_file = self.app.pargs.cache_file
         date = self.app.pargs.date
@@ -79,7 +78,7 @@ class WarpRun(DataRun):
         cache_file_ = None
         if(cache_file):
             cache_file_ = os.path.expanduser(cache_file)
-        client = WarpClass(config=self.app.config)
+        client = cls(config=self.app.config)
         response = client.cache_policy(
             account,
             date,
@@ -90,155 +89,69 @@ class WarpRun(DataRun):
         client.parse(response)
         self.app.render(client.to_ddh())
 
-class Billing(DataRun):
-    def cache(self, cache_file):
-        if(not cache_file):
-            raise NotImplementedError # We will add fetching later.
-        self.app.log.info("Using cached response: {cache}".format(cache=cache_file))
-        with open(cache_file, "r") as f:
-            reader = csv.DictReader(f)
-            header = reader.fieldnames
-            data = [[row[col] for col in header] for row in reader]
-        return DDH(header=header, data=data)
-
-    def run(self, **kwargs):
-        cache_file = self.app.pargs.cache_file
-        out = self.cache(cache_file)
-        self.app.render(out)
-        return out
-
 class BillingMonthly(DataRun):
     class Meta:
         label = "billing-monthly"
         description = "Get the monthly billing meta information."
 
     def run(self, **kwargs):
-        account = self.app.pargs.account
-        cache_file = self.app.pargs.cache_file
-        date = self.app.pargs.date
-        expire_cache = self.app.pargs.expire_cache
-        client = MonthlyInvoice(config=self.app.config)
-        response = client.cache_policy(
-            account,
-            date,
-            cache_file,
-            expire_cache,
-            log=self.app.log,
-        )
-        client.parse(response)
-        self.app.render(client.to_ddh())
+        self.run_call(MonthlyInvoice, **kwargs)
 
-class BillingLineItems(Billing):
-    class Meta:
-        label = "billing-line-items"
-        description = "Get the line items billing meta information."
-
-class BillingLineItemAggregates(Billing):
-    class Meta:
-        label = "billing-line-item-aggregates"
-        description = "Get the billing line item aggregate totals."
-
-class ComputeAV(DataRun):
-    class Meta:
-        label = "compute-av"
-        description = "Get the AV of instance meta information"
-
-        arguments = DataRun.Meta.arguments + [(
-            ["--compute-details"], dict(
-                type=str,
-                help="The path to the cached compute-details response to use."
-            )
-        )]
-
-    def run(self, **kwargs):
-        cache_file = self.app.pargs.cache_file
-        compute_details = self.app.pargs.compute_details
-        if(not cache_file):
-            raise NotImplementedError # We will add fetching later.
-        self.app.log.info("Using cached response: {cache}".format(cache=cache_file))
-        if(not compute_details):
-            raise NotImplementedError
-        self.app.log.info("Using compute_details response: {compute_details}".format(compute_details=compute_details))
-        out = compute_av(cache_file, compute_details)
-        self.app.render(out)
-        return out
-
-class ServiceRequestsRun(DataRun):
-    class Meta:
-        label = "service-requests"
-        description = "get the detailed service requests meta information."
-
-    def run(self, **kwargs):
-        account = self.app.pargs.account
-        cache_file = self.app.pargs.cache_file
-        date = self.app.pargs.date
-        expire_cache = self.app.pargs.expire_cache
-        client = ServiceRequests(config=self.app.config)
-        response = client.cache_policy(
-            account,
-            date,
-            cache_file,
-            expire_cache,
-            log=self.app.log,
-        )
-        client.parse(response)
-        self.app.render(client.to_ddh())
-
-class ComputeDetails(WarpRun):
+class ComputeDetails(DataRun):
     class Meta:
         label = "compute-details"
         description = "Get the detailed instance meta information."
 
     def run(self, **kwargs):
-        self.warp_run(ComputeDetailsWarp, **kwargs)
+        self.run_call(ComputeDetailsWarp, **kwargs)
 
-class ComputeMigration(WarpRun):
+class ComputeMigration(DataRun):
     class Meta:
         label = "compute-migration"
         description = "Get the migration recommendations meta information"
 
     def run(self, **kwargs):
-        self.warp_run(ComputeMigrationWarp, **kwargs)
+        self.run_call(ComputeMigrationWarp, **kwargs)
 
-class ComputeRI(WarpRun):
+class ComputeRI(DataRun):
     class Meta:
         label = "compute-ri"
         description = "Get the ri recommendations meta information."
 
     def run(self, **kwargs):
-        self.warp_run(ComputeRIWarp, **kwargs)
+        self.run_call(ComputeRIWarp, **kwargs)
 
-class ComputeUnderutilized(WarpRun):
+class ComputeUnderutilized(DataRun):
     class Meta:
         label = "compute-underutilized"
         description = "Get the underutilized instance meta information"
 
     def run(self, **kwargs):
-        self.warp_run(ComputeUnderutilizedWarp, **kwargs)
+        self.run_call(ComputeUnderutilizedWarp, **kwargs)
 
-class ComputeUnderutilizedBreakdown(WarpRun):
+class ComputeUnderutilizedBreakdown(DataRun):
     class Meta:
         label = "compute-underutilized-breakdown"
         description = "Get the underutilized instance breakdown meta information"
 
     def run(self, **kwargs):
-        self.warp_run(ComputeUnderutilizedBreakdownWarp, **kwargs)
+        self.run_call(ComputeUnderutilizedBreakdownWarp, **kwargs)
 
-class DBDetails(WarpRun):
+class DBDetails(DataRun):
     class Meta:
         label = "db-details"
         description = "Get the detailed rds meta information"
 
     def run(self, **kwargs):
-        self.warp_run(DBDetailsWarp, **kwargs)
+        self.run_call(DBDetailsWarp, **kwargs)
 
-class DBIdle(WarpRun):
+class DBIdle(DataRun):
     class Meta:
         label = "db-idle"
         description = "List idle database instances."
 
     def run(self, **kwargs):
-        self.warp_run(DBIdleWarp, **kwargs)
+        self.run_call(DBIdleWarp, **kwargs)
 
 class Domains(DataRun):
     class Meta:
@@ -285,29 +198,89 @@ class IAMUsers(DataRun):
         self.app.render(out)
         return out
 
-class LBIdle(WarpRun):
+class LBIdle(DataRun):
     class Meta:
         label = "lb-idle"
         description = "List idle load balancers."
 
     def run(self, **kwargs):
-        self.warp_run(LBIdleWarp, **kwargs)
+        self.run_call(LBIdleWarp, **kwargs)
 
-class RIPricings(WarpRun):
+class RIPricings(DataRun):
     class Meta:
         label = "ri-pricings"
         description = "Get the detailed ri pricings meta information."
 
     def run(self, **kwargs):
-        self.warp_run(RIPricingWarp, **kwargs)
+        self.run_call(RIPricingWarp, **kwargs)
 
-class StorageDetached(WarpRun):
+class ServiceRequestsRun(DataRun):
+    class Meta:
+        label = "service-requests"
+        description = "get the detailed service requests meta information."
+
+    def run(self, **kwargs):
+        self.run_call(ServiceRequests, **kwargs)
+
+class StorageDetached(DataRun):
     class Meta:
         label = "storage-detached"
         description = "List detached storage volumes."
 
     def run(self, **kwargs):
-        self.warp_run(StorageDetachedWarp, **kwargs)
+        self.run_call(StorageDetachedWarp, **kwargs)
+
+class Billing(DataRun):
+    def cache(self, cache_file):
+        if(not cache_file):
+            raise NotImplementedError # We will add fetching later.
+        self.app.log.info("Using cached response: {cache}".format(cache=cache_file))
+        with open(cache_file, "r") as f:
+            reader = csv.DictReader(f)
+            header = reader.fieldnames
+            data = [[row[col] for col in header] for row in reader]
+        return DDH(header=header, data=data)
+
+    def run(self, **kwargs):
+        cache_file = self.app.pargs.cache_file
+        out = self.cache(cache_file)
+        self.app.render(out)
+        return out
+
+class BillingLineItems(Billing):
+    class Meta:
+        label = "billing-line-items"
+        description = "Get the line items billing meta information."
+
+class BillingLineItemAggregates(Billing):
+    class Meta:
+        label = "billing-line-item-aggregates"
+        description = "Get the billing line item aggregate totals."
+
+class ComputeAV(DataRun):
+    class Meta:
+        label = "compute-av"
+        description = "Get the AV of instance meta information"
+
+        arguments = DataRun.Meta.arguments + [(
+            ["--compute-details"], dict(
+                type=str,
+                help="The path to the cached compute-details response to use."
+            )
+        )]
+
+    def run(self, **kwargs):
+        cache_file = self.app.pargs.cache_file
+        compute_details = self.app.pargs.compute_details
+        if(not cache_file):
+            raise NotImplementedError # We will add fetching later.
+        self.app.log.info("Using cached response: {cache}".format(cache=cache_file))
+        if(not compute_details):
+            raise NotImplementedError
+        self.app.log.info("Using compute_details response: {compute_details}".format(compute_details=compute_details))
+        out = compute_av(cache_file, compute_details)
+        self.app.render(out)
+        return out
 
 __ALL__ = [
     ZephyrData,

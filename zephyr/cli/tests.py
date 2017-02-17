14ae27293269792900558cc49fa3ffa8c94a6ca9
Dakota Blair <dblair@logicworks.net>
2017-02-17 22:41:36 +0100

    Added fixture loading and deleting to tests. Next step is to write tests based on the fixtures.

--- zephyr/cli/tests.py
+++ zephyr/cli/tests.py
@@ -1,6 +1,21 @@
+import os
+import sqlite3
+
 from cement.utils import test
 
 from ..__main__ import Zephyr
+from ..core.fixtures import test_account
+from ..core.utils import get_config_values, ZephyrException
+
+def get_db_path():
+    with Zephyr() as app:
+        config = app.config
+    zephyr_config_keys = ("ZEPHYR_CACHE_ROOT", "ZEPHYR_DATABASE")
+    cache_root, db = [
+        os.path.expanduser(path)
+        for path in get_config_values("zephyr", zephyr_config_keys, config)
+    ]
+    return os.path.join(cache_root, db)
 
 class TestZephyr(Zephyr):
     class Meta:
@@ -12,6 +27,85 @@ class TestZephyr(Zephyr):
             with obj.assertRaises(SystemExit) as cm:
                 app.run()
             obj.eq(cm.exception.code, 0, msg="Expected to return SystemExit: 0")
+
+    @classmethod
+    def assert_zephyr_expected_failure(cls, obj, args):
+        with cls(argv=args) as app:
+            with obj.assertRaises(ZephyrException) as exc:
+                app.run()
+            obj.eq(
+                bool(exc.exception.args[0]),
+                True,
+                msg="Expected an appropriate error message."
+            )
+
+class TestZephyrFixtures(test.CementTestCase):
+    app_class = TestZephyr
+
+    @classmethod
+    def _delete_fixtures(cls, cur):
+        for table in test_account.keys():
+            cur.execute("""
+                DELETE FROM {} WHERE "index"=-1
+            """.format(table))
+
+    @classmethod
+    def _load_fixtures(cls, cur):
+        cls._delete_fixtures(cur)
+        cur.execute("""
+            INSERT INTO cc_accounts values (:index, :aws_account, :id, :name)
+        """, test_account["cc_accounts"])
+        cur.execute("""
+            INSERT INTO lo_accounts values (:index, :id, :name)
+        """, test_account["lo_accounts"])
+        cur.execute("""
+            INSERT INTO sf_accounts values (:index, :Id, :Name, :Type)
+        """, test_account["sf_accounts"])
+        cur.execute("""
+            INSERT INTO sf_aws values (
+                :index,
+                :Name,
+                :Acct_Number__c,
+                :Assoc_Project__c,
+                :Cloudcheckr_ID__c,
+                :Cloudcheckr_Name__c,
+                :Bitdefender_ID__c
+            )
+        """, test_account["sf_aws"])
+        cur.execute("""
+            INSERT INTO sf_projects values (
+                :index,
+                :Id,
+                :Name,
+                :Account__c,
+                :Dynamics_ID__c,
+                :JIRAKey__c,
+                :LogicOps_ID__c,
+                :Main_Project_POC__c,
+                :MRR__c,
+                :Planned_Spend__c
+            )
+        """, test_account["sf_projects"])
+
+    @classmethod
+    def setUpClass(cls):
+        with sqlite3.connect(get_db_path()) as con:
+            cls._load_fixtures(con.cursor())
+
+    @classmethod
+    def tearDownClass(cls):
+        with sqlite3.connect(get_db_path()) as con:
+            cls._delete_fixtures(con.cursor())
+
+    def test_one(self):
+        TestZephyr.assert_zephyr_expected_failure(self, [
+            "report",
+            "account-review",
+            "--account=..",
+        ])
+
+    def test_two(self):
+        return True
 
 class TestZephyrBase(test.CementTestCase):
     app_class = TestZephyr
---
+++ zephyr/core/fixtures.py
@@ -0,0 +1,40 @@
+test_account = {
+    "cc_accounts" : {
+        "index" : -1,
+        "aws_account" : "aws_id",
+        "id" : "1,729",
+        "name" : "Test Account"
+    },
+    "lo_accounts" : {
+        "index" : -1,
+        "id" : "536904",
+        "name" : "Logicworks R&D"
+    },
+    "sf_accounts" : {
+        "index" : -1,
+        "Id" : "sf_account_id",
+        "Name" : "Test Account",
+        "Type" : "Active"
+    },
+    "sf_aws" : {
+        "index" : -1,
+        "Name" : ".meta",
+        "Acct_Number__c" : "aws_id",
+        "Assoc_Project__c" : "",
+        "Cloudcheckr_ID__c" : "",
+        "Cloudcheckr_Name__c" : "",
+        "Bitdefender_ID__c" : "bd_id"
+    },
+    "sf_projects" : {
+        "index" : -1,
+        "Id" : "sf_project_id",
+        "Name" : "Test Account",
+        "Account__c" : "sf_account_id",
+        "Dynamics_ID__c" : "LOGICWORKSRND",
+        "JIRAKey__c" : "ZEP",
+        "LogicOps_ID__c" : "536904",
+        "Main_Project_POC__c" : "",
+        "MRR__c" : "",
+        "Planned_Spend__c" : "100000.0"
+    }
+}
